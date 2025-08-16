a473cb2094d2b96e87f1fceca7fb42e47f75638a
nonatee <inctut7@gmail.com>
2025-08-16 05:37:17 +0300

    movable rect implemented

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,32 @@ fn main() {
 }
 
 #[derive(Default)]
-struct EGuiApp {}
+struct EGuiApp {
+    position: Pos2,
+}
 
 impl EGuiApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
-        Self::default()
+        Self {
+            position: Pos2::new(400.0, 400.0),
+        }
     }
 }
-
+use egui::{Color32, Pos2, Rect, Sense, Stroke, Vec2};
 impl eframe::App for EGuiApp {
+    
    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-       egui::CentralPanel::default().show(ctx, |ui| {
-           ui.heading("Hello World!");
-       });
+   egui::CentralPanel::default().show(ctx, |ui| {
+    let rect = ui.allocate_rect(
+        egui::Rect::from_min_size(self.position, egui::vec2(100.0, 80.0)),
+        egui::Sense::drag(),
+    );
+    ui.painter().rect(rect.rect, 10.0, egui::Color32::LIGHT_BLUE, egui::Stroke::new(2.0, egui::Color32::BLACK), egui::StrokeKind::Middle);
+
+    if rect.dragged() {
+        println!("Rectangle clicked!");
+        self.position += rect.drag_delta();
+    }
+});
    }
 }
